cf90eb68a734d580092b6831ea3a17a084abd993
matheusvogel <matheus_faria@sicredi.com.br>
2022-08-01 15:00:42 -0300

    autenticação e hash nas senhas

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController
+{
+    public function index()
+    {
+        return view('login.index');
+    }
+
+    public function store(Request $request)
+    {
+        if(!Auth::attempt(['email' => $request->email, 'password' => $request->senha])) {
+            return redirect()->back()->withErrors('Cliente ou senha inválidos');
+        }
+
+        return redirect('/cadastrar-emprestimo');
+     }
+}
